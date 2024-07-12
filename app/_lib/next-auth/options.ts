18e096fb856df032ca775207c4a56869748b303c
rala-rationallife <s.tsuduki26@gmail.com>
2024-07-12 09:46:45 +0900

    Prisma migrate

---
+++ app/_lib/next-auth/options.ts
@@ -0,0 +1,27 @@
+import { NextAuthOptions } from "next-auth";
+import GitHubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/_lib/prisma";
+import { Adapter } from "next-auth/adapters";
+
+export const nextAuthOptions: NextAuthOptions = {
+  debug: false,
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma) as Adapter,
+  callbacks: {
+    session: ({ session, user }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: user.id,
+        },
+      };
+    },
+  },
+};
--- app/page.tsx
+++ app/page.tsx
@@ -55,15 +55,13 @@ const books = [
 // eslint-disable-next-line @next/next/no-async-client-component
 export default function Home() {
   return (
-    <>
-      <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
-        <h2 className="text-center w-full font-bold text-3xl mb-2">
-          Book Commerce
-        </h2>
-        {books.map((book) => (
-          <Book key={book.id} book={book} />
-        ))}
-      </main>
-    </>
+    <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
+      <h2 className="text-center w-full font-bold text-3xl mb-2">
+        Book Commerce
+      </h2>
+      {books.map((book) => (
+        <Book key={book.id} book={book} />
+      ))}
+    </main>
   );
 }
